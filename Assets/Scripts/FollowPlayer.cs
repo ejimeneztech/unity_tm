50b93b9b80ffa9a0aaf433ab4094977f1c4ff885
Emilio Jimenez <ejimenez@DiamondGame.com>
2024-04-18 16:55:55 -0700

    added nav mesh and mesh agents for enemies

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if (player == null)
+        {
+            Debug.LogError("Missing Player Reference");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
